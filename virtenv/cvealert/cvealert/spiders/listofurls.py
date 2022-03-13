fff078992de3cec0784d4a465af91cf7dca7e6e1
Tyler Taylor <tyler@databrokers.io>
2022-03-13 15:29:42 -0500

    Built Database.
    - Created a scripts to create the 400+ urls needed for scraping.
    - Ran databasebuilder.py and output to database.csv. (110,000+ lines)
    - Still need to clean output....

---
+++ virtenv/cvealert/cvealert/spiders/listofurls.py
@@ -0,0 +1,6 @@
+x = 1
+
+while x < 405:
+    print(f'"https://www.cvedetails.com/vulnerability-list.php?vendor_id=0&product_id=0&version_id=0&page={x}&hasexp=0&opdos=0&opec=0&opov=0&opcsrf=0&opgpriv=0&opsqli=0&opxss=0&opdirt=0&opmemc=0&ophttprs=0&opbyp=0&opfileinc=0&opginf=0&cvssscoremin=8&cvssscoremax=10&year=0&month=0&cweid=0&order=1&trc=20163&sha=73ae8ed7437a717dd008af338bd9870e7c2c9241"'",")
+    x += 1
+
